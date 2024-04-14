2401c9fcfb2b68847e5f4aa6814ca6740ec3da6b
kokonline <hbencheikh003@gmail.com>
2024-04-14 14:58:34 +0100

    Initial Commit

---
+++ onetouch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  onetouch
+//
+//  Created by hoxon on 10/6/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ onetouch/onetouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  onetouchApp.swift
+//  onetouch
+//
+//  Created by hoxon on 10/6/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct onetouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
